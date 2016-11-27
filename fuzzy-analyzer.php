1d742b36868fe83f84e8d8801851187d080a4c54
godot <godot@bk.ru>
2016-11-28 01:55:07 +0400

    ready for first use

--- fuzzy-analyzer.php
+++ fuzzy-analyzer.php
@@ -16,71 +16,66 @@ if ($_SERVER['REQUEST_METHOD'] === "POST") {
 
         $features = array();
         $items = array();
-        try{
-        foreach($features_json as $feature_json){
-            $mem_function = NULL;
-            if(array_key_exists("trimf", $feature_json)){
-                $trimf_json = $feature_json['trimf'];
-                $a = $trimf_json['a'];
-                $b = $trimf_json['b'];
-                $c = $trimf_json['c'];
 
-                $mem_function = new Trimf($a, $b, $c);
+        try {
+            foreach ($features_json as $feature_json) {
+                $mem_function = NULL;
+                if (array_key_exists("trimf", $feature_json)) {
+                    $trimf_json = $feature_json['trimf'];
+                    $a = $trimf_json['a'];
+                    $b = $trimf_json['b'];
+                    $c = $trimf_json['c'];
+
+                    $mem_function = new Trimf($a, $b, $c);
+                }
+                $feature = new Feature($feature_json['identifier'], $mem_function);
+                array_push($features, $feature);
             }
-            $feature = new Feature($feature_json['identifier'], $mem_function);
-            array_push($features, $feature);
-        }
 
-        foreach($items_json as $item_json){
-            $identifier = $item_json['identifier'];
-            $feature_values = $item_json['feature_values'];
+            foreach ($items_json as $item_json) {
+                $identifier = $item_json['identifier'];
+                $feature_values = $item_json['feature_values'];
 
-            $item = new Item($identifier, $feature_values);
+                $item = new Item($identifier, $feature_values);
 
-            array_push($items, $item);
-        }
+                array_push($items, $item);
+            }
 
-        $aggregation_function = get_aggregation_function_by_key($aggregation);
+            $aggregation_function = get_aggregation_function_by_key($aggregation);
 
-        $analyzer = new Analyzer($features, $items, $aggregation_function);
-        $results = $analyzer->analyze();
-
-        $response = new SuccessResponse();
-        $response->status = ResponseConstants::success_message;
-        $response->status_code = ResponseConstants::success_code;
-        $response->name = $name;
-        $response->results = $results;
-        $response->suggested_item = $analyzer->suggest_best();
-
-        echo json_encode($response, JSON_UNESCAPED_UNICODE);
-        }
-        catch(Exception $e){
-            $error_response = new ErrorResponse();
-            $error_response->status_code = ResponseConstants::not_json_error_code;
-            $error_response->status_code = ResponseConstants::not_json_error_message;
-            $error_response->error_message = $e->getMessage();
+            $analyzer = new Analyzer($features, $items, $aggregation_function);
+            $results = $analyzer->analyze();
+//            var_dump($results);
+            $response = new SuccessResponse();
+            $response->status = ResponseConstants::success_message;
+            $response->status_code = ResponseConstants::success_code;
+            $response->name = $name;
+            $response->results = $results;
+            $response->suggested_item = $analyzer->suggest_best();
 
             echo json_encode($response, JSON_UNESCAPED_UNICODE);
+        } catch (Exception $e) {
+            $error_response = new ErrorResponse();
+            $error_response->status_code = ResponseConstants::not_json_error_code;
+            $error_response->status = ResponseConstants::not_json_error_message;
+            $error_response->error_message = $e->getMessage();
+
+            echo json_encode($error_response, JSON_UNESCAPED_UNICODE);
         }
 
 
-
-
-
-    }
-    else{
+    } else {
         $error_response = new ErrorResponse();
         $error_response->status_code = ResponseConstants::not_json_error_code;
-        $error_response->status_code = ResponseConstants::not_json_error_message;
+        $error_response->status = ResponseConstants::not_json_error_message;
 
-        echo json_encode($response, JSON_UNESCAPED_UNICODE);
+        echo json_encode($error_response, JSON_UNESCAPED_UNICODE);
     }
 
-}
-else{
+} else {
     $error_response = new ErrorResponse();
     $error_response->status_code = ResponseConstants::not_post_error_code;
-    $error_response->status_code = ResponseConstants::not_post_error_message;
+    $error_response->status = ResponseConstants::not_post_error_message;
 
-    echo json_encode($response, JSON_UNESCAPED_UNICODE);
+    echo json_encode($error_response, JSON_UNESCAPED_UNICODE);
 }
