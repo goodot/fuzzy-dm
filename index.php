43c802fb9ebd856f3f50f2523f4630d1a2d6fd84
godot <godot@bk.ru>
2016-11-27 23:20:11 +0400

    added request example

--- index.php
+++ index.php
@@ -10,7 +10,6 @@ $membership_function = new Trimf(160, 215, 240);
 $feature = new Feature("სიმაღლე", $membership_function, 198);
 //var_dump($feature);
 $ginobili = new Item("Manu Ginobili", array($feature, new Feature("ასაკი", new Trimf(10, 28, 45), 39)), new ArithmeticMean());
-echo json_encode($ginobili);
 //var_dump($ginobili);
 //echo $ginobili->aggregate();
 //$player1 = new Item(PLAYER, )
