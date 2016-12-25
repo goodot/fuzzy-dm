64f30cb8a7c64a7d8a6ae8dfb2ce6d7aa5211a90
Sergi Pasoev <s.pasoev@gmail.com>
2016-12-25 20:24:09 +0400

    membership functions done

---
+++ models/Feature.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ketili\models;
+use ketili\models\membership\MembershipFunction;
+
+class Feature
+{
+    public $identifier;
+    public $mem_function;
+    public $weight;
+
+
+    function __construct($identifier, $mem_function)
+    {
+        if ($mem_function instanceof MembershipFunction) {
+            $this->identifier = $identifier;
+            $this->mem_function = $mem_function;
+        } else {
+            throw new \Exception("mem_functon should be MembershipFunction");
+        }
+
+    }
+
+    function set_weight($weight)
+    {
+        if($weight >1 || $weight < 0)
+            throw new \Exception("weight must be in a range [0,1]");
+        $this->weight = $weight;
+    }
+
+    function get_weight()
+    {
+        return $this->weight;
+    }
+
+    function call_membership_function($value)
+    {
+        return $this->mem_function->call($value);
+    }
+}
---
+++ models/membership_functions/MembershipFunction.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace ketili\models\membership;
+
+
+interface MembershipFunction
+{
+    function call($x);
+}
