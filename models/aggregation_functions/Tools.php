06dbea7936a38e0aa751bd4b9be4c5350d5d12a3
Sergi Pasoev <s.pasoev@gmail.com>
2016-12-25 20:18:50 +0400

    tools done

--- models/aggregation_functions/Tools.php
+++ models/aggregation_functions/Tools.php
@@ -6,5 +6,42 @@ namespace ketili\models\aggregation;
 
 class Tools
 {
+    static function get_aggregation_function_by_key($key)
+    {
+        $aggregation_functions = array("arithmetic_mean", "geometric_mean",
+            "harmonic_mean", "weighted_arithmetic_mean", "weighted_geometric_mean",
+            "weighted_harmonic_mean");
+        if (in_array($key, $aggregation_functions)) {
 
+            switch ($key) {
+                case Constants::ArithmeticMean: {
+                    return new ArithmeticMean();
+                }
+                    break;
+                case Constants::GeometricMean: {
+                    return new GeometricMean();
+                }
+                    break;
+                case Constants::HarmonicMean: {
+                    return new HarmonicMean();
+                }
+                    break;
+                case Constants::WeightedArithmeticMean: {
+                    return new WeightedArithmeticMean();
+                }
+                    break;
+                case Constants::WeightedGeometricMean: {
+                    return new WeightedGeometricMean();
+                }
+                    break;
+                case Constants::WeightedHarmonicMean: {
+                    return new WeightedHarmonicMean();
+                }
+                    break;
+                default: {
+                    return new ArithmeticMean();
+                }
+            }
+        } else throw new \Exception("unknown key for aggregation function");
+    }
 }
