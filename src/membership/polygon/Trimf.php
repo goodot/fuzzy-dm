b1db8f0f302930d9a0b607a0cc4ef17455595460
g.jobadze <godot@bk.ru>
2017-03-17 12:14:51 +0400

    change namespace

--- src/membership/polygon/Trimf.php
+++ src/membership/polygon/Trimf.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace models\membership\polygon;
+namespace ketili\membership\polygon;
 
 
 
--- src/membership/sigmoid/SNonLinear.php
+++ src/membership/sigmoid/SNonLinear.php
@@ -7,7 +7,7 @@
  * To change this template use File | Settings | File Templates.
  */
 
-namespace models\membership\sigmoid;
+namespace ketili\membership\sigmoid;
 
 class SNonLinear extends   Sigmoid{
     function call($x)
