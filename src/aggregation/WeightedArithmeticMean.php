c897e9ac07f38cd03bd99550c67d261af9b75633
Goderdzi Jobadze <godot@bk.ru>
2017-06-21 02:07:31 +0400

    change again in weighted arithmetic mean

--- src/aggregation/WeightedArithmeticMean.php
+++ src/aggregation/WeightedArithmeticMean.php
@@ -10,7 +10,7 @@ class WeightedArithmeticMean implements WeightedAggregationFunction
         $multiplied_array = Operations::array_multiplication($array, $weights);
         $sum = array_sum($weights);
         $sum_multiplied_array = array_sum($multiplied_array);
-        return $sum_multiplied_array;
+        return $sum_multiplied_array/$sum;
 
     }
 }
